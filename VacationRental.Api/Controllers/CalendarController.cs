0e0b2d884dcb44634f7e7abd71e752b91326fa3f
Victor Bejinariu <Victor.Bejinariu@nuvei.com>
2023-01-15 00:41:01 +0200

    Fixed CalendarController Tests for the moment

--- VacationRental.Api/Controllers/CalendarController.cs
+++ VacationRental.Api/Controllers/CalendarController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using VacationRental.Api.Models;
+using VacationRental.Application.Abstractions;
 
 namespace VacationRental.Api.Controllers
 {
@@ -10,15 +11,15 @@ namespace VacationRental.Api.Controllers
     [ApiController]
     public class CalendarController : ControllerBase
     {
-        private readonly IDictionary<int, RentalViewModel> _rentals;
-        private readonly IDictionary<int, BookingViewModel> _bookings;
+        private readonly IRentalRepository _rentalRepository;
+        private readonly IBookingRepository _bookingRepository;
 
         public CalendarController(
-            IDictionary<int, RentalViewModel> rentals,
-            IDictionary<int, BookingViewModel> bookings)
+            IRentalRepository rentalRepository,
+            IBookingRepository bookingRepository)
         {
-            _rentals = rentals;
-            _bookings = bookings;
+            _rentalRepository = rentalRepository??throw new ArgumentNullException(nameof(rentalRepository));
+            _bookingRepository = bookingRepository??throw new ArgumentNullException(nameof(bookingRepository));
         }
 
         [HttpGet]
@@ -26,7 +27,8 @@ namespace VacationRental.Api.Controllers
         {
             if (nights < 0)
                 throw new ApplicationException("Nights must be positive");
-            if (!_rentals.ContainsKey(rentalId))
+            var rental = await _rentalRepository.GetById(rentalId);
+            if (rental ==null)
                 throw new ApplicationException("Rental not found");
 
             var result = new CalendarViewModel 
@@ -42,7 +44,7 @@ namespace VacationRental.Api.Controllers
                     Bookings = new List<CalendarBookingViewModel>()
                 };
 
-                foreach (var booking in _bookings.Values)
+                foreach (var booking in await _bookingRepository.Get())
                 {
                     if (booking.RentalId == rentalId
                         && booking.Start <= date.Date && booking.Start.AddDays(booking.Nights) > date.Date)
